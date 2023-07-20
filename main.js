e84e32fc722374ee4138d4c933bdb73706166731
lathamadakkannu <lathamadakkannu@gmail.com>
2023-07-19 19:44:17 -0700

    addnew

---
+++ main.js
@@ -0,0 +1,13 @@
+var arr=[11,10,70,75,59,2,9];
+document.write("arr=[11,10,70,75,59,2,9]" + "<br>" + "<br>");
+
+	for(let i=0; i<arr.length; i++)
+	{
+		if((arr[i]%3==0)&&(arr[i]%5==0)){
+	
+			document.write(arr[i]);
+									}
+	}
+		
+		
+	 
